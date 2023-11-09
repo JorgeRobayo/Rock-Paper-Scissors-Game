6c6a6fda79d6c21442ffb4920f194d7b908804ca
jorgerobayo <jorgerobmor@gmail.com>
2023-11-09 14:16:35 -0500

    cpu random result set

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 //                      {{GAME IDEAS}}
 
-//Player will be able to hover over the button options
+//Buttons: the user will be able to hover over the button options
 //and see the shape displayed
+
+//when the player is not hovering over any option the shape will go into a neutral shape(rock)
 
 //once the player makes a choice the game will start.
 
 //ROUNDS: The best of 3 rounds wins the game
 
+//ANIMATIONS: while the game is starting there will be a  'shake' animation
+
 //RESTART: once the game is finished it will be restarted automatically
+//before the game restarts there will be a timer of 5seconds 
 
 //LOG HISTORY: this will display the History of the different rounds
 
@@ -23,7 +28,7 @@
 //PLAYER2: add a second user player in the future.
 
 
-//                          {{GAME LOGIC}}
+//                          {{GAME LOGIC OVERVIEW}}
 
 //call all query selectors first
 //
@@ -62,10 +67,33 @@ function changeResultImg(choice){
     //the line below will change the machine result image but is not needed.
     // machineImage.src = `images/${choice}.png`
 
-    //this console log will show that the functions is set properly
+    //this console log will show that the function is set properly
     // console.log('works')
 }
 
+
+//This function will loop over each button 
+optionBtns.forEach((button, index) => {
+
+    //this is adding an event listener which is adding functionality to each optionBtn
+    button.addEventListener("click", (e) => {
+        
+        //this will choose a random number between 0 and 2 (3 indexes in total)
+        let randomNumber = Math.floor(Math.random() * 3) //<==(3 indexes in total)
+        
+        //this will print a random number when any optionBtn is clicked
+        //it will proof that the function is working correctly
+        // console.log(randomNumber)
+        
+        //Here I am creating an array of images that the machine can choose from
+        let machineImg = ['images/rock.png', 'images/paper.png', 'images/scissors.png']
+        
+        //this is changing the src of the image based the outcome of the random math
+        machineResult.src =  machineImg[randomNumber]
+
+
+    })
+})
 
 
 
